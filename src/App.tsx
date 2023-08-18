eeb93dd7805e1c05ea323905c615eefd64a41235
Aman <amansahani.200@gmail.com>
2023-08-19 01:05:38 +0530

    Made some routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route, Link, Outlet, useParams } from 'react-router-dom';
+import Home from './home/home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dark:bg-black dark:text-white flex justify-center items-center min-h-screen text-xl font-semibold">
+      <Router>
+        <Routes>
+          <Route path='/' Component={Home}/>
+          <Route path='/prep' Component={learn}>
+            <Route path='ml' Component={ml} >  </Route>
+            <Route path='gate' Component={gate} />
+            <Route path='extra' element={<Extract/>} >
+            <Route path=':id' ></Route>
+            </Route>
+          </Route>        
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+const learn = () => (
+  <div className='dark:text-white'>
+    <Link className='text-cyan-300'  to={'/prep/ml'}>
+      <li><p>ML</p></li>
+    </Link>
+    <Outlet/>
+    <Link to={'/prep/gate'} className='text-cyan-300'>
+      <li><p>GATE</p></li>
+    </Link>
+    
+  </div>
+)
+const ml = () =>(
+  <div>
+    <h1>ML</h1>
+  </div>
+)
+const gate = () =>(
+  <div>
+    <h1>GATE</h1>
+  </div>
+)
+const  Extract = () =>{
+  const {id} = useParams();
+  return(
+    <div>
+      <p>Extra {id}</p>
+    </div>
+  )
+}
